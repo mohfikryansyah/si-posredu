2198638c788678c3fcb18215ce634159b9cd4e23
mohfikryansyah <moh.fikryansyah@gmail.com>
2024-09-28 13:45:30 +0800

    coba schedule

--- routes/console.php
+++ routes/console.php
@@ -4,26 +4,29 @@ use Carbon\Carbon;
 use App\Models\Pelayanan;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\Scheduling\Schedule;
+// use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('schedule:run', function (Schedule $schedule) {
-    // Ambil tanggal pelayanan bulan ini
-    $tanggalPelayanan = Pelayanan::whereMonth('tanggal_pelayanan', Carbon::now()->month)
-                                ->whereYear('tanggal_pelayanan', Carbon::now()->year)
-                                ->value('tanggal_pelayanan');
+// Artisan::command('schedule:run', function (Schedule $schedule) {
+//     // Ambil tanggal pelayanan bulan ini
+//     $tanggalPelayanan = Pelayanan::whereMonth('tanggal_pelayanan', Carbon::now()->month)
+//                                 ->whereYear('tanggal_pelayanan', Carbon::now()->year)
+//                                 ->value('tanggal_pelayanan');
 
-    // Jika ada jadwal pelayanan untuk bulan ini
-    if ($tanggalPelayanan) {
-        // Parse tanggal pelayanan untuk mendapatkan hari dan waktu yang dibutuhkan
-        $tanggalPelayanan = Carbon::parse($tanggalPelayanan);
+//     // Jika ada jadwal pelayanan untuk bulan ini
+//     if ($tanggalPelayanan) {
+//         // Parse tanggal pelayanan untuk mendapatkan hari dan waktu yang dibutuhkan
+//         $tanggalPelayanan = Carbon::parse($tanggalPelayanan);
 
-        // Jika hari ini adalah tanggal pelayanan, jadwalkan command
-        if (Carbon::now()->isSameDay($tanggalPelayanan)) {
-            $schedule->command('pelayanan:check-missed')->everySecond(); // atau waktu yang kamu inginkan
-        }
-    }
-});
+//         // Jika hari ini adalah tanggal pelayanan, jadwalkan command
+//         if (Carbon::now()->isSameDay($tanggalPelayanan)) {
+//             $schedule->command('pelayanan:check-missed')->everySecond(); // atau waktu yang kamu inginkan
+//         }
+//     }
+// });
+
+Schedule::command('pelayanan:check-missed')->everySecond();
